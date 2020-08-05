7f3272ea70461e8ae578471548961fa3fd383761
Anna Shirokova <anja.shirokova@automatic.com>
2020-08-05 20:28:39 +0200

    Initial Commit

---
+++ MotionTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MotionTest
+//
+//  Created by Anna Shirokova on 05/08/2020.
+//  Copyright © 2020 Anna Shirokova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
